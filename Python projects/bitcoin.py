7b97fd9abc39a8e214dd2aab4daada91a76d4f47
mustafahhh <87324977+mustafahhh@users.noreply.github.com>
2021-08-08 14:12:21 +0300

    Update bitcoin.py

--- Python projects/bitcoin.py
+++ Python projects/bitcoin.py
@@ -13,6 +13,7 @@ style.use('fivethirtyeight')
 fig=plt.figure()
 ax1=fig.add_subplot(1,1,1)
 bitcoinprice=[]
+
 def foo(bar):
     print('hello {}'.format(bar))
     return r.get(f'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms={bar}').json()[bar]
@@ -25,10 +26,10 @@ def ex():
         res = res.result()
         print(res)
     e=Label(root,text="^^Bitcoin price^^",fg="white",bg="#272727",font=("Courier",20))
-    e.place(x=100,y=200)
+    e.place(x=120,y=200)
 
     s=Label(root,text=res,fg="white",bg="#272727",font=("Courier",15))
-    s.place(x=150,y=175)
+    s.place(x=200,y=175)
     root.after(1000,ex)
 def limitSizeDay(*args):
     values = value.get()
@@ -52,7 +53,7 @@ def trans(*args):
 value = StringVar()
 value.trace('w', limitSizeDay)
 value.trace('w',xx)
-label = Label(root,text="Enter currency kind",fg="white",bg="#272727",font=("Courier",20))
+label = Label(root,text="Enter currency shortcut(like USD)",fg="white",bg="#272727",font=("Courier",20))
 currency=Entry(root,bg="white",textvariable=value,fg="#272727",font=("Courier", 19))
 currency.place(x=100,y=100,height=50,width=200)
 label.place(x=50,y=50)
@@ -60,6 +61,6 @@ button= Button(root,text="enter",fg="#272727",bg="orange",bd=0,font=("Courier",1
 button.place(x=300,y=100,width=50,height=50)
 button.bind('<Button-1>',trans)
 root.configure(bg="#272727")
-root.geometry("500x400")
+root.geometry("579x400")
 root.mainloop()
 

7b4eb12597fd1f7e0db13ffa9095c8c238f0aeb1
mustafahhh <87324977+mustafahhh@users.noreply.github.com>
2021-08-08 13:58:48 +0300

    Add files via upload

---
+++ Python projects/Dhook.py
@@ -0,0 +1,6 @@
+from dhooks import Webhook
+
+hook = Webhook('https://discord.com/api/webhooks/866469850409599016/n_tgLjjbiQn01TCBnYxZ1OAZlI7p4QDbm-gR5MueIeLyKuthL28XW0qwMk84iGTcgzMB')
+
+hook.send("""
+          """)
